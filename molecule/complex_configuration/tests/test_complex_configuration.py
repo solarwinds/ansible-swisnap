bf1085d471efb3c987162dde445a658f68fb1e54
d-maslyk <dominik.maslyk@solarwinds.com>
2021-06-14 14:40:42 +0200

    Enable support of newest Ubuntu 21.04 and 20.04 (AO-17661) (#10)

--- molecule/complex_configuration/tests/test_complex_configuration.py
+++ molecule/complex_configuration/tests/test_complex_configuration.py
@@ -31,10 +31,10 @@ def test_swinsapd_service(host):
 
 def test_process_swisnapd(host):
     assert host.process.get(user=SOLARWINDS, comm=SWISNAPD)
-    # checking collector plugins: aosystem, processes, logs, bridge, apache
-    assert len(host.process.filter(user=SOLARWINDS, comm="snap-plugin-col")) == 5
-    # checking default publisher plugins: publisher-appoptics v1, publisher-appoptics v2, publisher-processes
-    assert len(host.process.filter(user=SOLARWINDS, comm="snap-plugin-pub")) == 3
+    # checking collector plugins: logs, bridge, apache
+    assert len(host.process.filter(user=SOLARWINDS, comm="snap-plugin-col")) == 3
+    # checking default publisher plugins: publisher-appoptics v1, publisher-appoptics v2
+    assert len(host.process.filter(user=SOLARWINDS, comm="snap-plugin-pub")) == 2
 
 
 def test_sockets(host, major_version):
--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -31,10 +31,10 @@ def test_swinsapd_service(host):
 
 def test_process_swisnapd(host):
     assert host.process.get(user=SOLARWINDS, comm=SWISNAPD)
-    # checking default collector plugins: aosystem, processes, logs
-    assert len(host.process.filter(user=SOLARWINDS, comm="snap-plugin-col")) == 3
-    # checking default publisher plugins: publisher-appoptics, publisher-processes
-    assert len(host.process.filter(user=SOLARWINDS, comm="snap-plugin-pub")) == 2
+    # checking default collector plugin: logs
+    assert len(host.process.filter(user=SOLARWINDS, comm="snap-plugin-col")) == 1
+    # checking default publisher plugin: publisher-appoptics
+    assert len(host.process.filter(user=SOLARWINDS, comm="snap-plugin-pub")) == 1
 
 
 def test_sockets(host, major_version):
